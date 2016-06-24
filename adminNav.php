c63f14052dc250c0c5dbc066bf557116df7508f3
Erica G <G2xDezignz@users.noreply.github.com>
2016-06-24 09:21:53 -0500

    Modified Acct role
    Modified so that Acct role can see all reports and add reports; removed ability to see users

--- adminNav.php
+++ adminNav.php
@@ -1,7 +1,7 @@
 <div id="adminNav">
 <p><a href="main.php">Home</a> 
 <?php 
-	if ($_SESSION['MM_UserGroup']=='adm' || $_SESSION['MM_UserGroup']=='amadm' || $_SESSION['MM_UserGroup']=='astmgr' || $_SESSION['MM_UserGroup']=='acct') { 
+	if ($_SESSION['MM_UserGroup']=='adm' || $_SESSION['MM_UserGroup']=='amadm' || $_SESSION['MM_UserGroup']=='astmgr') { 
 ?> 
  | <a href="users.php">User Admin</a>
 <?php } ?> 
@@ -16,7 +16,7 @@
  | <a href="categories.php">Report Group Admin</a>
 <?php } ?> 
 <?php 
-	if ($_SESSION['MM_UserGroup']=='adm' || $_SESSION['MM_UserGroup']=='amadm' || $_SESSION['MM_UserGroup']=='astmgr') { 
+	if ($_SESSION['MM_UserGroup']=='adm' || $_SESSION['MM_UserGroup']=='amadm' || $_SESSION['MM_UserGroup']=='astmgr' || $_SESSION['MM_UserGroup']=='acct') { 
 ?> 
  | <a href="reports.php">Reports Admin</a>
  <?php } ?>
